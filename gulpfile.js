e15e727b07ee2e6b0be9ba82745e401a5ca53163
Kevin Hill <kevinkhill@gmail.com>
2015-09-18 12:23:57 -0700

    yay gulp!

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,47 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-   bower = require('gulp-bower'),
-  notify = require('gulp-notify'),
-     run = require('gulp-run'),
-  rename = require('gulp-rename');
+  var gulp = require('gulp'),
+      sass = require('gulp-ruby-sass'),
+     bower = require('gulp-bower'),
+    notify = require('gulp-notify'),
+     shell = require('gulp-shell'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+sourcemaps = require('gulp-sourcemaps'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+   process = require('process');
 
-var jekyll = new run.Command('jekyll');
+//process.env.BROWSERIFYSHIM_DIAGNOSTICS=1;
+
+var browserify = require('browserify');
+var debowerify = require('debowerify');
+var browserify_shim = require('browserify-shim');
 
 var config = {
     sassPath: './resources/sass',
     bowerDir: './bower_components'
 };
 
+gulp.task('browserify', function() {
+    return browserify({
+            debug: true,
+            entries: './resources/js/app.js',
+            transform: [
+                browserify_shim,
+                debowerify
+            ]
+        })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(rename({
+            extname: ".min.js"
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./_site/js'));
+});
 
 gulp.task('bower', function() {
     return bower().pipe(gulp.dest(config.bowerDir));
@@ -20,6 +50,16 @@ gulp.task('bower', function() {
 gulp.task('icons', function() {
     return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
                .pipe(gulp.dest('./_site/fonts'));
+});
+
+gulp.task('images', function() {
+    return gulp.src('./resources/images/**')
+               .pipe(imagemin({
+                    optimizationLevel: 5,
+                    progressive: true,
+                    interlaced: true
+                }))
+               .pipe(gulp.dest('./_site/images'));
 });
 
 gulp.task('css', function() {
@@ -35,15 +75,17 @@ gulp.task('css', function() {
     .on("error", notify.onError(function (error) {
         return "Error: " + error.message;
     }))
+    .pipe(sourcemaps.init())
     .pipe(rename({
         extname: ".min.css"
     }))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./_site/css'));
 });
 
-gulp.task('jekyll', function() {
-    run('jekyll build -d /var/www/lavadocs.local/public');
-});
+gulp.task('jekyll', shell.task([
+    'jekyll build -d /var/www/lavadocs.local/public'
+]));
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
