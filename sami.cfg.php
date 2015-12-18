0de86debd92f9bcb4a213765dcbfb4ea96ce5601
Kevin Hill <kevinkhill@gmail.com>
2015-12-17 18:44:39 -0800

    adding sami back

--- sami.cfg.php
+++ sami.cfg.php
@@ -1,26 +1,38 @@
 <?php
 
 use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
 use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
+
+$dir = realpath(__DIR__.'/../../php/lavacharts/src');
 
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
     ->exclude('Laravel')
     //->exclude('Traits')
-    ->in(__DIR__.'/../src')
-;
+    ->in($dir);
 
-$versions = GitVersionCollection::create(__DIR__)
-    ->addFromTags('2.5.2')
-    ->add('traits', 'v3.0 branch');
+$versions = GitVersionCollection::create($dir)
+    ->add('2.5', '2.5.x Branch')
+    ->add('3.0', '3.0.x Branch');
 
-return new Sami($iterator, [
-    'theme'                => 'default',
+$options = [
+    'theme'                => 'lava',
     'versions'             => $versions,
     'title'                => 'Lavacharts API',
-    'build_dir'            => __DIR__.'/../build/%version%',
-    'cache_dir'            => __DIR__.'/../cache/%version%',
+    'build_dir'            => __DIR__.'/api/%version%',
+    'cache_dir'            => __DIR__.'/.cache/%version%',
+    'remote_repository'    => new GitHubRemoteRepository('khill/lavacharts', dirname($dir)),
     'default_opened_level' => 2,
-]);
+];
+
+$sami = new Sami($iterator, $options);
+
+$templates = $sami['template_dirs'];
+$templates[] = __DIR__ . '/themes/';
+
+$sami['template_dirs'] = $templates;
+
+return $sami;
