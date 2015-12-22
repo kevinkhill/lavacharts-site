3326808e8f04c7c9a376168a81037a39155de252
Kevin Hill <kevinkhill@gmail.com>
2015-12-22 04:50:03 -0800

    api theme updates

--- sami.cfg.php
+++ sami.cfg.php
@@ -10,29 +10,22 @@ $dir = realpath(__DIR__.'/../../php/lavacharts/src');
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->exclude('Laravel')
-    //->exclude('Traits')
+    //->exclude('Laravel')
     ->in($dir);
 
 $versions = GitVersionCollection::create($dir)
-    ->add('2.5', '2.5.x Branch')
     ->add('3.0', '3.0.x Branch');
-
+    //->add('2.5', '2.5.x Branch');
+    
 $options = [
     'theme'                => 'lava',
     'versions'             => $versions,
     'title'                => 'Lavacharts API',
     'build_dir'            => __DIR__.'/_site/api/%version%',
     'cache_dir'            => __DIR__.'/.cache/%version%',
-    'remote_repository'    => new GitHubRemoteRepository('khill/lavacharts', dirname($dir)),
+    'template_dirs'        => [__DIR__.'/themes/'],
+    'remote_repository'    => new GitHubRemoteRepository('kevinkhill/lavacharts', dirname($dir)),
     'default_opened_level' => 2,
 ];
 
-$sami = new Sami($iterator, $options);
-
-$templates = $sami['template_dirs'];
-$templates[] = __DIR__ . '/themes/';
-
-$sami['template_dirs'] = $templates;
-
-return $sami;
+return new Sami($iterator, $options);
