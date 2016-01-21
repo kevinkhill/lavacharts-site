149dd3b9231c6b713d862891cbf6d233b3ec46ac
Kevin Hill <kevinkhill@gmail.com>
2016-01-21 11:53:41 -0800

    removing files not pertaining to the site and some housekeeping

--- sami.cfg.php
+++ sami.cfg.php
@@ -6,7 +6,7 @@ use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
 $dir = realpath(__DIR__.'/../../php/lavacharts/src');
-
+echo $dir;exit;
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
@@ -17,14 +17,16 @@ $versions = GitVersionCollection::create($dir)
     ->add('3.0', '3.0 Branch')
     ->add('2.5', '2.5 Branch');
 
+$githubRepo = new GitHubRemoteRepository('kevinkhill/lavacharts', dirname($dir));
+
 $options = [
     'theme'                => 'lava',
     'versions'             => $versions,
     'title'                => 'Lavacharts API',
     'build_dir'            => __DIR__.'/_site/api/%version%',
     'cache_dir'            => __DIR__.'/.cache/%version%',
-    'template_dirs'        => [__DIR__.'/themes/'],
-    'remote_repository'    => new GitHubRemoteRepository('kevinkhill/lavacharts', dirname($dir)),
+    'template_dirs'        => [__DIR__.'/resources/sami-themes/'],
+    'remote_repository'    => $githubRepo,
     'default_opened_level' => 2,
 ];
 
